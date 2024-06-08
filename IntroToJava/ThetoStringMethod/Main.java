b32bda2e577a09c32fb18e146a6a0b1c67309368
Stone L <stonelai11@gmail.com>
2024-06-08 13:51:11 -0600

    toString() Method

---
+++ IntroToJava/ThetoStringMethod/Main.java
@@ -0,0 +1,10 @@
+package IntroToJava.ThetoStringMethod;
+
+public class Main {
+    public static void main(String[] args) {
+      Store lemonadeStand = new Store("Lemonade", 3.75);
+      Store cookieShop = new Store("Cookies", 5);
+      System.out.println(lemonadeStand);
+      System.out.println(cookieShop);
+    }
+  }
