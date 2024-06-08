ddca47a6c50dbbf765fbf69d60d7ace23ae2ad0a
Stone L <stonelai11@gmail.com>
2024-06-08 12:54:43 -0600

    Returns

--- IntroToJava/Returns/Main.java
+++ IntroToJava/Returns/Main.java
@@ -1,5 +1,9 @@
 package IntroToJava.Returns;
 
 public class Main {
-    
-}
+    public static void main(String[] args) {
+      Store lemonadeStand = new Store("Lemonade", 3.75);
+      double lemonadePrice = lemonadeStand.getPriceWithTax();
+      System.out.println(lemonadePrice);
+    }
+  }
--- IntroToJava/Returns/Store.java
+++ IntroToJava/Returns/Store.java
@@ -1,5 +1,29 @@
 package IntroToJava.Returns;
 
 public class Store {
-    
-}
+    // instance fields
+    String productType;
+    double price;
+    double totalPrice;
+    double tax = 0.08;
+      
+    // constructor method
+    public Store(String product, double initialPrice) {
+      productType = product;
+      price = initialPrice;
+    }
+      
+    // increase price method
+    public void increasePrice(double priceToAdd){
+      double newPrice = price + priceToAdd;
+      price = newPrice;
+    }
+      
+    // get price with tax method
+    public double getPriceWithTax() {
+      totalPrice = price + price * tax;
+      return totalPrice;
+    }
+  
+  }
+  
