a1d11a592bbcf4ac515d57e044c49d38636e698e
Stone L <stonelai11@gmail.com>
2024-06-08 01:15:17 -0600

    Completed Lesson

---
+++ IntroToJava/ReassigningInstanceFields/Main.java
@@ -0,0 +1,9 @@
+package IntroToJava.ReassigningInstanceFields;
+
+public class Main {
+  public static void main(String[] args) {
+    Store lemonadeStand = new Store("Lemonade", 3.75);
+    lemonadeStand.increasePrice(1.50);
+    System.out.println(lemonadeStand.price);
+  }
+}
