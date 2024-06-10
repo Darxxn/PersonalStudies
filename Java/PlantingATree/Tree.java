fafb99c8af6565d8789f4d87115cd2b8280f30b3
Stone L <stonelai11@gmail.com>
2024-06-10 14:36:50 -0600

    Java

---
+++ Java/PlantingATree/Tree.java
@@ -0,0 +1,14 @@
+package Java.PlantingATree;
+
+public class Tree {
+    public static void main(String[] args) {
+        //We are going to introduce ourselves and print a tree to the screen
+        System.out.println("Hey there, I'm Stone");
+        System.out.println("I am going to plant a tree today!");
+        System.out.println("  *  ");
+        System.out.println(" *** ");
+        System.out.println(" *** ");
+        System.out.println("  *  ");
+        System.out.println("  *  ");
+      }
+}
