e48bc46db497bd5657aa3381321a616be845c063
Stone L <stonelai11@gmail.com>
2024-06-10 18:58:49 -0600

    Else If

---
+++ Java/If-Then-Else-If/ElseIf.java
@@ -0,0 +1,48 @@
+public class ElseIf {
+    boolean isFilled;
+    double billAmount;
+    String shipping;
+    
+    public ElseIf(boolean filled, double cost, String shippingMethod) {
+          if (cost > 24.00) {
+        System.out.println("High value item!");
+      }
+      isFilled = filled;
+      billAmount = cost;
+      shipping = shippingMethod;
+    }
+    
+    public void ship() {
+      if (isFilled) {
+        System.out.println("Shipping");
+        System.out.println("Shipping cost: " + calculateShipping());
+      } else {
+        System.out.println("Order not ready");
+      }
+    }
+    
+    public double calculateShipping() {
+           // declare conditional statement here
+      if (shipping == "Regular") {
+        return 0;
+      }
+  
+      else if (shipping == "Express") {
+        return 1.75;
+      }
+  
+      else {
+        return .50;
+      }
+       }
+    
+    public static void main(String[] args) {
+      // do not alter the main method!
+      ElseIf book = new ElseIf(true, 9.99, "Express");
+      ElseIf chemistrySet = new ElseIf(false, 72.50, "Regular");
+      
+      book.ship();
+      chemistrySet.ship();
+    }
+  }
+  
