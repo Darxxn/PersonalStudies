fd5f54653565d6be9ace4cc87246f949278a5e49
Stone L <stonelai11@gmail.com>
2024-06-17 14:10:57 -0600

    Importing Arrays

---
+++ Java/ArraysAndArraylists/Arrays/ImportingArrays/Main.java
@@ -0,0 +1,11 @@
+package Java.ArraysAndArraylists.Arrays.ImportingArrays;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Newsfeed sampleFeed = new Newsfeed();
+        String[] topics = sampleFeed.getTopics();
+        System.out.println(Arrays.toString(topics));
+    }
+}
---
+++ Java/ArraysAndArraylists/Arrays/ImportingArrays/Newsfeed.java
@@ -0,0 +1,13 @@
+package Java.ArraysAndArraylists.Arrays.ImportingArrays;
+
+public class Newsfeed {
+    public Newsfeed(){
+        
+    }
+        
+    public String[] getTopics(){
+      String[] topics = {"Opinion", "Tech", "Science", "Health"};
+      return topics;
+    }
+  }
+    
