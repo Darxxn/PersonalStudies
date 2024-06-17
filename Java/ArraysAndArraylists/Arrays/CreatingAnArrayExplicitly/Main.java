6650f879d0e60be16775236c8da08faaa1d2fa63
Stone L <stonelai11@gmail.com>
2024-06-17 14:05:33 -0600

    Creating An Array Explicitly

---
+++ Java/ArraysAndArraylists/Arrays/CreatingAnArrayExplicitly/Main.java
@@ -0,0 +1,9 @@
+package Java.ArraysAndArraylists.Arrays.CreatingAnArrayExplicitly;
+
+public class Main {
+    public static void main(String[] args) {
+      Newsfeed sampleFeed = new Newsfeed();
+      String[] topics = sampleFeed.getTopics();
+      System.out.println(topics);
+    }
+  }
