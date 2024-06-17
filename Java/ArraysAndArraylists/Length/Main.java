fee469bf1288a4b8f86e88bc95aa2a5276dca88a
Stone L <stonelai11@gmail.com>
2024-06-17 15:05:31 -0600

    Length

---
+++ Java/ArraysAndArraylists/Length/Main.java
@@ -0,0 +1,8 @@
+package Java.ArraysAndArraylists.Length;
+
+public class Main {
+  public static void main(String[] args){
+    Newsfeed sampleFeed = new Newsfeed();
+    System.out.println("The number of topics is "+ sampleFeed.getNumTopics());
+  }
+}
