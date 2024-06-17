b31dbf251760031e0f10cc85d63f8b568f7caa1a
Stone L <stonelai11@gmail.com>
2024-06-17 14:50:41 -0600

    Elements by Index

---
+++ Java/ArraysAndArraylists/Arrays/GetElementsByIndex/Newsfeed.java
@@ -0,0 +1,24 @@
+package Java.ArraysAndArraylists.Arrays.GetElementsByIndex;
+
+public class Newsfeed {
+    String[] topics = {"Opinion", "Tech", "Science", "Health"};
+    public int[] views = {0, 0, 0, 0};
+    
+    public Newsfeed(){
+  
+    }
+      
+    public String[] getTopics(){
+      return topics;
+    }
+    
+    // Create getFirstTopic() below
+    public String getFirstTopic() {
+      return topics[0];
+    }
+  
+    
+    public void viewTopic(int topicIndex){
+      views[topicIndex] = views[topicIndex] + 1;
+    }
+  }
