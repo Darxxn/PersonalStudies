71a21cc676fb18acd9899fda2b5d54cf4039a841
Stone L <stonelai11@gmail.com>
2024-06-19 22:37:53 -0600

    Introduction - Adding to array won't work

---
+++ Java/ArraysAndArraylists/ArrayLists/Introduction/Shopping.java
@@ -0,0 +1,16 @@
+package Java.ArraysAndArraylists.ArrayLists.Introduction;
+
+class Shopping {
+  
+  public static void main(String[] args) {
+    
+    String[] groceryItems = {"steak", "milk", "jelly beans"};
+    double[] prices = {25.00, 2.95, 2.50};
+    
+    // Adding ham to the groceries
+    groceryItems[3] = "ham";
+    prices[3] = 4.99;
+    
+  }
+  
+}
