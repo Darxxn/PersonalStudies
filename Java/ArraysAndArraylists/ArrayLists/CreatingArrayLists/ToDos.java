caae66ce4fae67be156506a51fb9f8d5b3a41d38
Stone L <stonelai11@gmail.com>
2024-06-19 22:42:38 -0600

    Create ArrayList - Generics: symbol inside <>

---
+++ Java/ArraysAndArraylists/ArrayLists/CreatingArrayLists/ToDos.java
@@ -0,0 +1,14 @@
+package Java.ArraysAndArraylists.ArrayLists.CreatingArrayLists;
+
+import java.util.ArrayList;
+
+class ToDos {
+  
+  public static void main(String[] args) {
+    
+    // Create toDoList below:
+    ArrayList<String> toDoList = new ArrayList<String>();
+    
+  }
+  
+}
