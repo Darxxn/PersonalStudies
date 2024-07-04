c4159b272df16bd4512f46e8739cf274b963a444
Stone L <stonelai11@gmail.com>
2024-07-03 23:17:48 -0600

    While loop into For loop

---
+++ Java/Loops/UsingForLoops/Coffee.java
@@ -0,0 +1,18 @@
+// refractoring from while loop to for loop
+class Coffee {
+  
+    public static void main(String[] args) {
+      
+      // int cupsOfCoffee = 0;
+      
+      // while (cupsOfCoffee <= 100)
+      for (int i = 1; i <= 100; i++) {
+        
+        System.out.println("Fry drinks cup of coffee #" + i);
+        // cupsOfCoffee++;
+        
+      }
+      
+    }
+    
+  }
