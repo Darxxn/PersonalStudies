79e3c3744e50a874c66b77ef3663abe3f5f84376
Stone L <stonelai11@gmail.com>
2024-07-13 11:04:15 -0600

    .charAt()

---
+++ Java/StringMethods/charAt/Initials.java
@@ -0,0 +1,14 @@
+public class Initials {
+  
+	public static void main(String[] args) {
+    
+    // Add a first name and a last name:
+    String firstName = "Stone";  
+    String lastName = "Lai";
+    
+    // What are the initials?
+    System.out.println(firstName.charAt(0));
+    System.out.println(lastName.charAt(0));    
+  }
+  
+}
